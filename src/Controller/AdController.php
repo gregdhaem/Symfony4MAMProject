383d4bdddbdcff6dee1ed982920ae59f24a64af8
gregdhaem <gregdhaem@gmail.com>
2018-11-22 17:34:59 +0100

    formulaire images fin

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -50,7 +50,12 @@ class AdController extends AbstractController
 
 
         if($form -> isSubmitted() && $form -> isValid())
-        {
+        {   
+            foreach($ad -> getImages() as $image) {
+                $image -> setAd($ad);
+                $manager -> persist($image);
+            }
+
             $manager -> persist($ad);
             $manager -> flush();
             
--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -6,15 +6,24 @@ use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ImageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('url')
-            ->add('caption')
-        ;
+        -> add('url', UrlType::class, [
+            'attr' => [
+                'placeholder' => 'URL de la photo...'
+            ]
+        ])
+        -> add('caption', TextType::class, [
+            'attr' => [
+                'placeholder' => 'Titre de la photo...'
+            ]
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
