8dfefeb124d2c8954c8380fe3916d439e4f8caf8
gregdhaem <gregdhaem@gmail.com>
2018-11-22 14:19:07 +0100

    ajout du formulaire

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -26,19 +30,48 @@ class AdController extends AbstractController
     }
 
     /**
+     * Permet de créer une annonce
+     * 
+     * @Route("/ads/new", name="ads_create")
+     * 
+     * @return Response 
+     */
+    public function create(Request $request, ObjectManager $manager)
+    {   
+        $ad = new Ad();
+
+        $form = $this -> createForm(AdType::class, $ad);
+
+        $form -> handleRequest($request);
+
+        dump($ad);
+
+        if($form -> isSubmitted() && $form -> isValid())
+        {
+            $manager -> persist($ad);
+            $manager -> flush();
+            
+        }
+
+        return $this -> render('ad/new.html.twig', [
+            'form' => $form -> createView()
+        ]);
+    }
+
+    /**
      * Permet d'afficher une annonce
      * 
      * @Route("/ads/{slug}", name="ads_show")
      * 
      * @return Response
      */
-    public function show(AdRepository $repo, $slug)
+    public function show(Ad $ad) // Param Converter
     {
         // Récupération l'annonce qui correspond au slug !
-        $ad = $repo -> findOneBySlug($slug);
-
+        // $ad = $repo -> findOneBySlug($slug);
         return $this -> render('ad/show.html.twig', [
             'ad' => $ad
         ]);
     }
+
 }
