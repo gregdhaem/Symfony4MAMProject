c237cad12d4642b01c96f0f19641beeba8d300f9
gregdhaem <gregdhaem@gmail.com>
2018-12-13 08:54:32 +0100

    booking show ajouté

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,37 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller {
 
     /**
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello", name="hello_base")
-     * @Route("/hello/{prenom}", name="hello_prenom")
-     *
-     * Montre la page qui dit bonjour
-     * 
-     * @return void
-     */
-    public function hello($prenom = "anonyme", $age = ".") {
-        return $this->render(
-            'hello.html.twig',
-            [
-                'prenom' => $prenom,
-                'age' => $age
-            ]
-        );
-    }
-
-    // public function home(){
-    //    $prenoms =['Raphaël' => 10, 'Emilien' => 9, 'Kevin' => 14, 'Greg' => 58];
-    //    return $this->render(
-    //        "home.html.twig", 
-    //        [ 
-    //            'age' => 11,
-    //            'title' => "Bonjour à toutes",
-    //            'tableau' => $prenoms 
-    //        ]
-    //    );
-    // }
-
-    /**
      * @Route("/", name="homepage")
      */
     public function home(AdRepository $repo){
