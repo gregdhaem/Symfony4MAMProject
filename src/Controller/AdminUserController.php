b0f2e0a5b29660a6f697a0aa37800cabed938ccf
gregdhaem <gregdhaem@gmail.com>
2019-02-22 18:00:14 +0100

    added user admin section

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Service\PaginationService;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminUserController extends AbstractController
 {
     /**
-     * @Route("/admin/users", name="admin_users_index")
+     * @Route("/admin/users/{page<\d+>?1}", name="admin_users_index")
      */
-    public function index()
+    public function index(UserRepository $repo, $page, PaginationService $pagination)
     {
+        $pagination -> setEntityClass(User::class)
+                    -> setPage($page);
+
+
+
         return $this->render('admin/user/index.html.twig', [
-            'controller_name' => 'AdminUserController',
+            'pagination' => $pagination
+
         ]);
     }
 }
